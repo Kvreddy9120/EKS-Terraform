6272a299860872794e622e84df23a6a6b2f42b1b
Venkateswara Reddy Kuppireddy <kuppireddyvenkateswarareddy69@gmail.com>
2026-08-02 16:36:06 +0530

    Update and rename mani.tf to main.tf

--- main.tf
+++ main.tf
@@ -27,14 +27,21 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
 # ---------------------------------------------------------------------------
 # Networking data sources
 # ---------------------------------------------------------------------------
-data "aws_vpc" "default" {
-  default = true
-}
-
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [
+      "us-east-1a",
+      "us-east-1b",
+      "us-east-1c",
+      "us-east-1d",
+      "us-east-1f"
+    ]
   }
 }
 
